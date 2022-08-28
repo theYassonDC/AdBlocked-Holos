d4927859f3cdf2ce3d6f287a97faa6af86b6cd91
Juanda <98669750+theYassonDC@users.noreply.github.com>
2022-08-28 13:38:40 -0500

    one commit project extension

---
+++ extension/scripts/blocker.main.js
@@ -0,0 +1,2 @@
+/*br%*/console.log(/*br%*/"%cAD BLOCKED HABBO HOLOS v0.0.1", "color: #eee; font-size: 50px;",/*br%*/ "\nBloqueador de anuncios de habbo hoteles por JuandaC extension en fase beta gracias por usarla! \n\n Mi github: https://github.com/theYassonDC\n Instagram: @ljuanda_castro"/*br""%*/);document.getElementById("client-banner").hidden=true;document.getElementById("AdBox2").hidden=true;console.log("%cBLOCKER ~","color: yellow;", " Hidden is true public!");
+// Code of JuandaC view ~ https://github.com/theYassonDC
