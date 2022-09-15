938c350c21f218aba20f7ee2a1bf68caf69e4e0a
Juanda <98669750+theYassonDC@users.noreply.github.com>
2022-09-15 14:59:22 -0500

    New function detected from blocked públic

--- extension/scripts/blocker.main.js
+++ extension/scripts/blocker.main.js
@@ -1,2 +1,14 @@
-/*br%*/console.log(/*br%*/"%cAD BLOCKED HABBO HOLOS v0.0.1", "color: #eee; font-size: 50px;",/*br%*/ "\nBloqueador de anuncios de habbo hoteles por JuandaC extension en fase beta gracias por usarla! \n\n Mi github: https://github.com/theYassonDC\n Instagram: @ljuanda_castro"/*br""%*/);document.getElementById("client-banner").hidden=true;document.getElementById("AdBox2").hidden=true;console.log("%cBLOCKER ~","color: yellow;", " Hidden is true public!");
-// Code of JuandaC view ~ https://github.com/theYassonDC :D
+"use strict"
+console.log("%cAD BLOCKED HABBO HOLOS v0.0.1.0", "color: #eee; font-size: 50px;", "\nBloqueador de anuncios de habbo hoteles por JuandaC extension en fase beta gracias por usarla! \n\n Mi github: https://github.com/theYassonDC\n Instagram: @ljuanda_castro");
+function BlockedHolo(element) {
+    if (!element) {
+        console.log("%cBLOCKER ~", "color: yellow;", "Public detected..");
+    } else {
+        element.hidden = true;document.body.removeChild(element);console.log("%cBLOCKER ~", "color: yellow;", " Hidden is true public!");
+    }
+};
+// --> Kubbo.city and Hgalaxy.net ads
+let adbox = document.getElementById("AdBox2");
+let client_bannel = document.getElementById("client-banner");
+BlockedHolo(client_bannel);
+BlockedHolo(adbox);
